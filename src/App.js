268ba99d448adaf6a603a9edb6c145c081b97687
ddock4you <ddock4you@gmail.com>
2019-12-09 23:05:45 +0900

    - life cycling 사용

--- src/App.js
+++ src/App.js
@@ -6,27 +6,22 @@ import React from 'react';
 class App extends React.Component {
   
   state = {
-    count: 0
+    isLoading: true
+  }
+
+  componentDidMount = () => {
+    setTimeout(()=> {
+      this.setState({isLoading: false});
+    },3000);
   }
   
-  // state 값을 직접 바꾸는게 아니라 새롭게 선언(state는 object) update 느낌?
-  // setState는 state값을 새로 선언하고 render 함수에 refresh를 요청
-  add = () => {
-    this.setState(current => ({count: current.count +1}));
-    // this.setState({count: this.state.count +1});
-  };
-
-  minus = () => {
-    this.setState(current => ({count: current.count -1}));
-  };
+  
   
   render() {
+    const {isLoading} = this.state;
+
     return (
-      <div>
-        <h1>this number is {this.state.count}</h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
+      <div>{isLoading ? "Loading..." : "not Loading"}</div> 
     )
   }
 }
