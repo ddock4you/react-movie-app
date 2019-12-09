43c432424951b7016abc8569bb1b30207a4e09bf
ddock4you <ddock4you@gmail.com>
2019-12-09 17:10:35 +0900

    - component 추가 테스트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Test from './Test';
 
 function App() {
   return (
+    <div>
       <h1>hello!</h1>
+      <Test />
+    </div>
   );
 }
 
---
+++ src/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Test () {
+    return (
+        <div>
+            한 번 더 hello
+        </div>
+    )
+}
+
+export default Test;
