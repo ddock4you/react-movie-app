88cd4b3002c6879a2a006a22be5ac3de61c68ed9
ddock4you <ddock4you@gmail.com>
2019-12-09 18:32:55 +0900

    - propTypes 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-function Test ({name, picture}) {
+function Test ({name, picture, rating}) {
   return (
       <div>
+        <p>
           음식 이름: {name},<br/>
-          {name} 이미지 : <img src={picture} alt={name}/>
+        </p>
+        <p>
+          <img src={picture} alt={name}/> <br/>
+        </p>
+        <p>
+          점수: {rating} / 5<br/>
+        </p>
       </div>
   )
+}
+
+// propTypes : props의 속성값을 체크하는 플러그인
+Test.propTypes = {
+  name: propTypes.string.isRequired,
+  picture: propTypes.string.isRequired,
+  rating: propTypes.number.isRequired
 }
 
 // list 내 child들은 key value가 필요함
@@ -15,31 +30,36 @@ const foodLike = [
     id:1,
     name: "Kimchi",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+      rating: 1
   },
   {
     id:2,
     name: "Samgyeopsal",
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+      rating: 2
   },
   {
     id: 3,
     name: "Bibimbap",
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+      rating: 3
   },
   {
     id: 4,
     name: "Doncasu",
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+      rating: 4
   },
   {
     id: 5,
     name: "Kimbap",
     image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+      rating: 5
   }
 ]
 
@@ -48,7 +68,7 @@ function App() {
     <div>
       <h1>hello!</h1>
       {foodLike.map(dish => (
-        <Test key={dish.id} name={dish.name} picture={dish.image} />
+        <Test key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
       ))}
     </div>
   );
