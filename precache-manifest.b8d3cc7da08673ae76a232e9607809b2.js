2f7cd8834786ac3568dad547b826d14ef460c497
ddock4you <ddock4you@gmail.com>
2019-12-10 14:47:21 +0900

    Updates

---
+++ precache-manifest.b8d3cc7da08673ae76a232e9607809b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6544070cb4d85b6ce267619b36d182bc",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "daa24c7742b243500012",
+    "url": "/react-movie-app/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "29ad0bd8d8ed8629164f",
+    "url": "/react-movie-app/static/js/2.b552a36c.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react-movie-app/static/js/2.b552a36c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "daa24c7742b243500012",
+    "url": "/react-movie-app/static/js/main.b2279d3d.chunk.js"
+  },
+  {
+    "revision": "065ede9c8a91280c1dc0",
+    "url": "/react-movie-app/static/js/runtime-main.43864add.js"
+  }
+]);
